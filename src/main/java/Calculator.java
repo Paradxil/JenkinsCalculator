d0ef1f2cb01e45d640bfcd9a5b77b2bf2a30e2d0
Hunter Stratton <hstratton16@gmail.com>
2022-02-18 12:52:23 -0700

    Change UID implementation.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,4 @@
-import java.sql.Time;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
+import java.util.UUID;
 class Calculator {
 
     Calculator(){
@@ -75,12 +72,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        try {
-            TimeUnit.MICROSECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return n + System.currentTimeMillis();
+        return n + UUID.randomUUID().toString();
     }
 
 
